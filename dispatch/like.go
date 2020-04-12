82e6abc73209e7d4e36b306ee6e0e0057684363c
yankee <subyankee@gmail.com>
2020-04-12 17:46:16 +0900

    모든 페이지 추가 및 단일페이지 방식 추가

--- dispatch/like.go
+++ dispatch/like.go
@@ -19,9 +19,13 @@ type ReactionCounter struct {
 	Sum  int `json:"sum"`
 	Like int `json:"like"`
 }
+type ResponseEntry struct {
+	ReactionCounter   ReactionCounter `json:"reactionCounter"`
+	ReactionActivated string          `json:"reactionActivated"`
+}
 type Response struct {
 	Success bool `json:"success"`
-	content struct {
+	Content struct {
 		Id              string `json:"id"`
 		Type            string `json:"type"`
 		CategoryId      int    `json:"categoryId"`
@@ -32,9 +36,7 @@ type Response struct {
 	}
 }
 
-func EntryFetch(id string, entryId string) interface{} {
-	result := Response{}
-
+func EntryFetch(id string, entryId string, target *Response) Response {
 	url := fmt.Sprintf("https://%s.tistory.com/reaction", id)
 	data := []byte(fmt.Sprintf(`{"reactionType": "LIKE", "entryId": %s}`, entryId))
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
@@ -52,8 +54,40 @@ func EntryFetch(id string, entryId string) interface{} {
 
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &target)
+	//fmt.Printf("%+v\n", target)  // Do u want view of `Like Count` ?
+	return *target
+}
+
+func CheckEntryCount(id string, entryId string, target int) int {
+	result := ResponseEntry{}
+	blog := fmt.Sprintf("%s.tistory.com", id)
+	url := fmt.Sprintf("https://%s/reaction?entryId=%s", blog, entryId)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("Error Request... : ", err.Error())
+	}
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Whale/2.7.97.12 Safari/537.36")
+	req.Header.Set("Host", blog)
+
+	client := &http.Client{Timeout: time.Second * 10}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error Request... : ", err.Error())
+	} else if resp.StatusCode == 429 {
+		log.Panic("Too Many Request... Quit System")
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(body, &result)
-	fmt.Printf("%+v\n", result)
-	fmt.Println("debug")
-	return result
+	if result.ReactionCounter.Like >= target {
+		return target
+	}
+	fmt.Printf("%s\n", body)
+	fmt.Printf("entry : %s / Like : %d\n", entryId, result.ReactionCounter.Like)
+	return result.ReactionCounter.Like
 }
--- main.go
+++ main.go
@@ -2,20 +2,81 @@ package main
 
 import (
 	"LikeTistory/dispatch"
+	"flag"
 	"fmt"
+	"os"
+	"strconv"
+	"sync"
+	"time"
 )
 
 func main() {
 	//var clearIds []string
 	//url := "https://gmyankee.tistory.com/reaction"
-	id := "gmyankee"
-	ids := dispatch.PostParser(id)
-	for _, entryId := range ids {
-		fmt.Println(entryId)
-		res := dispatch.EntryFetch(id, entryId)
-		fmt.Printf("%+v\n", res)
-		fmt.Println("debug")
+	const (
+		InfoColor    = "\033[1;34m%s\033[0m"
+		NoticeColor  = "\033[1;36m%s\033[0m"
+		WarningColor = "\033[1;33m%s\033[0m"
+		ErrorColor   = "\033[1;31m%s\033[0m"
+		DebugColor   = "\033[0;36m%s\033[0m"
+	)
+	var (
+		wait sync.WaitGroup
+	)
+
+	blogId := flag.String("blog", "gmyankee", "<BlogId>.tistory.com")
+	single := flag.Bool("only", true, "Like only single post => true or false")
+	postId := flag.Int("Post Id", 200, "Single Mode is Required Post ID => <BlogId>.tistory.com/<PostId>")
+	target := flag.Int("target Like Count", 0, "Settings to Max Like Count")
+
+	if *single == true {
+		if *postId == 0 {
+			fmt.Printf(WarningColor, "\nSingle Mode is Required Post ID!\n")
+			os.Exit(3)
+		}
+		entryId := strconv.Itoa(*postId)
+		start := dispatch.CheckEntryCount(*blogId, entryId, *target)
+		if start == *target {
+			fmt.Printf(InfoColor, "Already Count!\n")
+			os.Exit(3)
+		}
+		for x := start; x < *target; x++ {
+			result := dispatch.Response{}
+			dispatch.EntryFetch(*blogId, entryId, &result)
+			if result.Content.ReactionCounter.Like > *target {
+				break
+			}
+		}
+		fmt.Printf(InfoColor, "Clear!\n")
+	} else {
+		fmt.Printf(WarningColor, "\nDue to heavy traffic, it can be blocked and interrupted.\n")
+		ids := dispatch.PostParser(*blogId)
+		wait.Add(len(ids))
+		completes := make([]string, len(ids))
+		go func() {
+			for _, entryId := range ids {
+				time.Sleep(time.Duration(2) * time.Second)
+				start := dispatch.CheckEntryCount(*blogId, entryId, *target)
+				if start == *target {
+					wait.Done()
+					continue
+				}
+				for x := start; x < *target; x++ {
+					fmt.Println(entryId)
+					result := dispatch.Response{}
+					dispatch.EntryFetch(*blogId, entryId, &result)
+					if result.Content.ReactionCounter.Like > *target {
+						completes = append(completes, entryId)
+						wait.Done()
+						break
+					}
+				}
+				wait.Done()
+			}
+		}()
 	}
+	wait.Wait()
+	fmt.Println("Complete")
 
 	//res := fetch(url, 291, result)
 
